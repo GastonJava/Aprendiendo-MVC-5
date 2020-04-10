e9568c1a670089d99e6074e4d6fce6d32735811c
gastonmirandajava@gmail.com <gastonmirandajava@gmail.com>
2020-04-10 14:04:10 -0300

    nuevo galeria

---
+++ Definitivo/Controllers/GaleriaController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Definitivo.Controllers
+{
+    public class GaleriaController : Controller
+    {
+
+        [AllowAnonymous]
+        public IActionResult Galeria()
+        {
+            return View();
+        }
+    }
+}
